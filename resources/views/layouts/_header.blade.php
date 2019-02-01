c79b38bbdbcfdf85fa315f904bddd54db3ddc332
qliu67 <qil231@eng.ucsd.edu>
2019-02-01 07:53:12 +0000

    Add routing name

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="/">Microblog</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Microblog</a>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
           <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
         </ul>
       </div>
