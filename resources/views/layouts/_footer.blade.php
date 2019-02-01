ceaf30c80cbcd6afdde10e17cdec70442341e520
qliu67 <qil231@eng.ucsd.edu>
2019-02-01 07:37:25 +0000

    Add header and footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://image.flaticon.com/icons/svg/25/25231.svg">
+  <a href="https://github.com/qliu67/microblog" target=_blank>
+    View source code
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >About</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,17 +5,14 @@
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+  @include('layouts._header')
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Microblog</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
-        </ul>
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
       </div>
-    </nav>
+    </div>
 
-  @yield('content')
 </body>
 </html>
