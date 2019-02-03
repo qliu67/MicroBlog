36da6e560dea1168df79febcc7a8941758e225d8
qliu67 <qil231@eng.ucsd.edu>
2019-02-03 06:27:04 +0000

    Add signup validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,13 @@ class UsersController extends Controller
     public function show(User $user) {
         return view('users.show', compact('user'));
     }
+
+    // store user information
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6']);
+        return;
+    }
 }
