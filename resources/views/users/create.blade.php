e39ed1db57a12cdb4f34a7fa9c421b393a50185a
qliu67 <qil231@eng.ucsd.edu>
2019-02-03 02:11:27 +0000

    Add signup form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'signup')
 
 @section('content')
-<h1>Signup to create your own microblog!</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>Sign Up</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="name">Username: </label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email: </label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password: </label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">Confirm Password: </label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
