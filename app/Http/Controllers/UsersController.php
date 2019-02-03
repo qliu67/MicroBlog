6bf1fc82c2eaadeba88736fa6fe46edd89828b5b
qliu67 <qil231@eng.ucsd.edu>
2019-02-03 01:52:07 +0000

    Add uder info page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     // create a user
     public function create() {
         return view('users.create');
+    }
+
+    // show user information
+    public function show(User $user) {
+        return view('users.show', compact('user'));
     }
 }
